0a316772431483ed9a3d22a0ae83e95353ec3238
yangzhuoxun <185755135@qq.com>
2017-05-10 11:36:10 +0800

    init code

---
+++ app/src/main/java/com/gome/fenghe/MainActivity.java
@@ -0,0 +1,40 @@
+package com.gome.fenghe;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import com.gome.fenghe.wifip2p.WiFiDirectActivity;
+
+public class MainActivity extends AppCompatActivity {
+    private static final String TAG = MainActivity.class.getSimpleName();
+
+    private Button mWifiBtn;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        initView();
+    }
+
+    private void initView() {
+        mWifiBtn = (Button) findViewById(R.id.wifi_p2p_btn);
+        mWifiBtn.setOnClickListener(mListener);
+    }
+
+
+    private View.OnClickListener mListener = new View.OnClickListener() {
+        @Override
+        public void onClick(View v) {
+            switch (v.getId()) {
+                case R.id.wifi_p2p_btn:
+                    startActivity(new Intent(MainActivity.this, WiFiDirectActivity.class));
+                    break;
+            }
+        }
+    };
+
+}
